dad144dcd972771094b08e81f3d5571e2776623b
Kuhelika Banerjee <kuhelikaban08@gmail.com>
2026-05-13 16:20:21 +0530

    Add background music functionality using THREE.js

---
+++ backgraound.js
@@ -0,0 +1,13 @@
+const listener = new THREE.AudioListener();
+camera.add(listener);
+
+const sound = new THREE.Audio(listener);
+
+const audioLoader = new THREE.AudioLoader();
+
+audioLoader.load('assets/music.mp3', function(buffer) {
+    sound.setBuffer(buffer);
+    sound.setLoop(true);
+    sound.setVolume(0.5);
+    sound.play();
+});
